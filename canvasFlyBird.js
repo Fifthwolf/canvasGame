0d90bc0109e89a220c9792114fa17362136a301f
fifthwolf <fifthwolf@qq.com>
2017-11-28 17:27:28 +0800

    update

--- canvasFlyBird.js
+++ canvasFlyBird.js
@@ -1,4 +1,19 @@
 var canvas = document.getElementById('canvas');
 
-canvas.width = 400;
-canvas.height = 600;
+var data = {
+  image: null
+}
+
+window.onload = function () {
+  imageLoaded();
+  //canvas.width = 400;
+  //canvas.height = 600;
+}
+
+function imageLoaded () {
+  var image = new Image();
+  image.src = 'flyBird.png';
+  image.onload = function () {
+    data.image = image;
+  }
+}
