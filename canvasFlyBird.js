46f4cbe407b19a06d70f0cd6c83aa0a8ac5c4c54
fifthwolf <fifthwolf@qq.com>
2017-11-29 15:55:00 +0800

    update

--- canvasFlyBird.js
+++ canvasFlyBird.js
@@ -1,7 +1,23 @@
 var canvas = document.getElementById('canvas');
 
 var data = {
-  image: null
+  image: null,
+  score: 0,
+  bird: {
+    color: 0, //0黄色，1蓝色，2红色
+    velocityY: 0,
+    left: 120,
+    top: 315,
+  },
+  system: {
+    refreshRate: 20, //刷新频率
+    start: false,
+    fail: false,
+  },
+  background: {
+
+  },
+  TIME: {}
 }
 
 window.onload = function () {
@@ -15,5 +31,17 @@ function imageLoaded () {
   image.src = 'flyBird.png';
   image.onload = function () {
     data.image = image;
+    drawImage();
   }
+}
+
+function drawImage () {
+  var context = canvas.getContext('2d');
+  data.TIME.drawImage = setInterval(function () {
+    drawBackground(context);
+  }, data.system.refreshRate);
+}
+
+function drawBackground (cxt) {
+  cxt.drawImage(data.image, 0, 0, 288, 512, 0, -55, 400, 711);
 }
