454ff5d76a2773040919df92a01412528c5a86f6
Lei Xia <fifthwolf@qq.com>
2018-04-30 11:58:52 +0800

    create new game

---
+++ arkanoid/arkanoid.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+  suitScreen();
+}
+
+function imageLoaded() {
+  var image = new Image();
+  image.src = 'arkanoid.png';
+  image.onload = function() {
+    loading.style.display = 'none';
+    _setCanvasProperty();
+    data.system.cxt = canvas.getContext('2d');
+    data.image = image;
+    data.system.time.previous = Date.now();
+  }
+
+  function _setCanvasProperty() {
+    canvas.width = 640;
+    canvas.height = 480;
+  }
+}
